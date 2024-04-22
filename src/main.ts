cd0b94d652c340405aba6f1ee96320f9a35d6ecc
GraffVishenka <65270494+GraffVishenka@users.noreply.github.com>
2024-04-22 09:23:40 +0500

    Final

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as cookieParser from "cookie-parser";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {cors: false});
+
+  app.use(cookieParser());
+
+  app.enableCors({ credentials: true, origin: true });
+
+  const config = new DocumentBuilder()
+    .setTitle("TaskManager")
+    .setDescription("TaskManager for ESoft")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup("swagger", app, document);
+
+  await app.listen(7777);
+}
+bootstrap();
